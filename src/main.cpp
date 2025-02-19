22678c1c86dbb0fa769164194a5279245bf07f00
Slaven Glumac <slaven.glumac@gmail.com>
2025-02-19 12:50:43 +0100

    Naming conventions in .clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,15 @@
 #include <span>
 
 int main(int argc, char **argv) {
+  const int maxArgCount = 1;
+  const int failureCode = 1;
   const std::span<char *> args(argv, static_cast<size_t>(argc));
-  if (argc != 2) {
+  if (argc != maxArgCount) {
     std::cerr << "Usage: " << args[0] << " <source-file>" << std::endl;
-    return 1;
+    return failureCode;
   }
 
   const std::optional<da4cpp::graph::DependencyGraph> symbolInfo = da4cpp::parsing::parse_translation_unit(args[1]);
 
-  return symbolInfo.has_value() ? 0 : 1;
+  return symbolInfo.has_value() ? 0 : failureCode;
 }
