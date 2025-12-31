53d890f32e7d38b035d7611ea0c0a1aee6946a99
Slaven Glumac <slaven.glumac@gmail.com>
2025-12-31 15:17:08 +0100

    Fix build_docs.sh

--- build_docs.sh
+++ build_docs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # pip install -r requirements_docs.txt
 REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOXYGEN_BUILD=$REPO/build/docs/doxygen
 DOCS=$REPO/docs
-DOCS_BUILD=$SCRIPT_DIR/docs/_build
+DOCS_BUILD=$REPO/docs/_build
 
 set -e  # Exit on any error
 
@@ -12,6 +13,7 @@ echo "Docs: $DOCS_DIR"
 # 1. Generate Doxygen XML
 echo "Generating Doxygen XML..."
 cd "$REPO"
+mkdir -p "$DOXYGEN_BUILD"
 doxygen Doxyfile
 
 # 2. Build Sphinx documentation
